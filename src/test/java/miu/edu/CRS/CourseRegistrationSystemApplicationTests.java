75e8a9ab8e4865410dd3e88da9da3eaefa8c5527
your name <simegnew62@gmail.com>
2022-12-17 22:23:46 -0600

    first spring inizialized folder

---
+++ src/test/java/miu/edu/CRS/CourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.CRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
